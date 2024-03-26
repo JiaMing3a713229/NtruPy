13698377c38caa170d5335917a29fef6394a0bbc
呂家明 <48340392+JiaMing3a713229@users.noreply.github.com>
2024-03-26 16:16:50 +0800

    NTRU-FTT

--- NtruCrypto/NTRUCryptoNp.py
+++ NtruCrypto/NTRUCryptoNp.py
@@ -1,64 +1,10 @@
 import math
-class PolyObj:
-    def __init__(self, name, coef):
+import numpy as np
 
-        self.poly_name = name
-        self.coef = coef
-        self.buf_size = len(coef)
-        self.degree = self.buf_size - 1
-        self.init_poly()
 
-    def init_poly(self):
+NTRU_N = 11
 
-        while(self.coef[self.degree] == 0 and self.degree > 0):
-            self.degree-=1 
-            
-    def print(self):
-        print(f"PolyObj {self.poly_name}: ", end="")
-        for c in self.coef[:-1]:
-            print(f"{c:^4d}", end="")
-        print(f"{self.coef[-1]:^4d} | degree: {self.degree}")
-
-    def println(self):
-        self.print_poly()
-        print(f"degree of {self.poly_name}: {self.degree}, buffer size: {self.buf_size}")
-    
-    def __repr__(self):
-        coef_str = ' '.join([f"{c:^4d}" for c in self.coef])
-        return f"PolyObj {self.poly_name}: {coef_str} | degree: {self.degree}, buffer size: {self.buf_size}"
-
-class Ntru1:
-
-    def __init__(self, N, p, q):
-        self.params = {
-            "N":N,
-            "p":p,
-            "q":q,
-            "fx": None,
-            "gx": None,
-            "ring": None,
-            "Fp": None,
-            "Fq": None,
-            "Kp": None
-        }
-
-        self.key_gen_flag = False
-
-    def poly(self, name, coef):
-        if(len(coef) == self.params["N"] + 1):
-            coef_0 = [0] * (self.params["N"] + 1)
-        else:
-            coef_0 = [0] * (self.params["N"])
-
-        for i in range(len(coef)):
-            coef_0[i] += coef[i]
-        return PolyObj(name, coef_0)
-
-    def ring(self, name):
-        coef_ring = [-1] + [0] * (self.params["N"] - 1) + [1]
-        return PolyObj(name, coef_ring)
-
-    def centered_zero(self, number, modulo):
+def centered_zero(number, modulo):
         num = number % modulo
         if num >= ((modulo + 1) // 2):
             num -= modulo
@@ -66,106 +12,168 @@ class Ntru1:
             num += modulo
         return num 
 
-    def gcd_of(self, a, b):
-        while b != 0:
-            a, b = b, a % b
-        return a
+def gcd_of(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def inv_of_num(num, mod_t):
+    tmp_m = mod_t
+    num %= mod_t
+    if gcd_of(num, mod_t) != 1:
+        print(f"Inverse of {num} mod {mod_t} does not exist")
+        return 0
+
+    d1, d2 = 0, 1
+    r = 0
+
+    while num != 0:
+        
+        q = int(mod_t / num)
+        r = mod_t % num
+        mod_t, num = num, r
+        d = d1 - q * d2
+        d1, d2 = d2, d
+        
+    return d1 if d1 > 0 else d1 + tmp_m
+
+def dec2bin(number, bits):
+    binary_array = [0] * bits
+    index = bits - 1
+    while number > 0 and index >= 0:
+        binary_array[index] = number % 2
+        number //= 2
+        index -= 1
+    return binary_array
+
+def dec2arr(number):
+    arr = [0] * NTRU_N
+    if number >= (1 << NTRU_N):
+        print("Error with number")
+        return None
+    elif number == 0:
+        return [0] * NTRU_N
+
+    bits = math.ceil(math.log2(number + 1))
+    index = bits - 1
+    bin_array = dec2bin(number, bits)
+    for i in range(bits):
+        arr[index - i] = bin_array[i]
+
+    return arr
+
+class PolyObj:
+    def __init__(self, name, coef):
+
+        self.poly_name = name
+        self.coef = np.array(coef)
+        self.buf_size = NTRU_N + 1 if (len(self.coef) > NTRU_N) else NTRU_N
+        self.degree = 0
+        self.init_poly()
+
+    def init_poly(self):
+        self.degree = self.buf_size - 1
+        while ((self.coef[self.degree]) == 0) and ((self.degree >= 1)):
+            self.degree = (self.degree - 1)
+
+    def print(self):
+        print(f"PolyObj {self.poly_name}: ", end="")
+        print(" ".join([f"{c:^4d}" for c in self.coef]), end="")
+        print(f" | degree: {self.degree} buffer size: {self.buf_size}")
     
-    def inv_of_num(self, num, mod_t):
-        tmp_m = mod_t
-        num %= mod_t
-        if self.gcd_of(num, mod_t) != 1:
-            print(f"Inverse of {num} mod {mod_t} does not exist")
-            return 0
+    def __repr__(self):
+        coef_str = " ".join([f"{c:^4d}" for c in self.coef])
+        return f"PolyObj {self.poly_name}: {coef_str} | degree: {self.degree}, buffer size: {len(self.coef)}"
 
-        d1, d2 = 0, 1
-        r = 0
+class Ntru:
 
-        while num != 0:
-            
-            q = int(mod_t / num)
-            r = mod_t % num
-            mod_t, num = num, r
-            d = d1 - q * d2
-            d1, d2 = d2, d
-            
-        return d1 if d1 > 0 else d1 + tmp_m
+    def __init__(self, N, p, q):
+        self.params = {
+            "N"     :N,
+            "p"     :p,
+            "q"     :q,
+            "fx"    : None,
+            "gx"    : None,
+            "ring"  : None,
+            "Fp"    : None,
+            "Fq"    : None,
+            "Kp"    : None
+        }
 
-    def dec2bin(self, number, bits):
-        binary_array = [0] * bits
-        index = bits - 1
-        while number > 0 and index >= 0:
-            binary_array[index] = number % 2
-            number //= 2
-            index -= 1
-        return binary_array
+        self.key_gen_flag = False
 
-    def dec2arr(self, number):
-        arr = [0] * self.params["N"]
-        if number >= (1 << self.params["N"]):
-            print("Error with number")
-            return None
-        elif number == 0:
-            return [0] * self.params["N"]
+    def poly(self, name, coef):
 
-        bits = math.ceil(math.log2(number + 1))
-        index = bits - 1
-        bin_array = self.dec2bin(number, bits)
-        for i in range(bits):
-            arr[index - i] = bin_array[i]
+        if len(coef) == self.params["N"] + 1:
+            coef_0 = [0] * (self.params["N"] + 1)
+        else:
+            coef_0 = [0] * self.params["N"]
 
-        return arr
+        for i in range(len(coef)):
+            coef_0[i] = coef[i]
+
+        return PolyObj(name, coef_0)
+
+    def ring(self, name):
+        coef_ring = [-1] + [0] * (self.params["N"] - 1) + [1]
+        return PolyObj(name, coef_ring)
 
     
-    def mulpoly(self, a:PolyObj, b:PolyObj, modulo_size, name):
-        ret_coef = [0] * self.params["N"]
-        for i in range(a.degree + 1):
-            for j in range(b.degree + 1):
-                ret_coef[(i + j) % self.params["N"]] += a.coef[i] * b.coef[j]
-                # ret_coef[(i + j) % self.params["N"]] %= modulo_size
-                ret_coef[(i + j) % self.params["N"]] = self.centered_zero(ret_coef[(i + j) % self.params["N"]], modulo_size)
+    def mulpoly(self, a, b, modulo_size, name):
+
+        # Convert coefficients to NumPy arrays
+        coef_a = np.array(a.coef)
+        coef_b = np.array(b.coef)
+        # Perform polynomial multiplication using NumPy's convolution
+        ret_coef = np.convolve(coef_a, coef_b, mode='full') % modulo_size
+        for i in range(self.params["N"], len(ret_coef)):
+            ret_coef[i % self.params["N"]] += ret_coef[i]
+        ret_coef = [centered_zero(ret_coef[c], modulo_size) for c in range(NTRU_N)]
         return PolyObj(name, ret_coef)
 
     def addpoly(self, a, b, modulo_size, name):
+
         ret_coef = [0] * self.params["N"]
-        for i in range(max(a.degree, b.degree) + 1):
-            ret_coef[i] = a.coef[i] + b.coef[i]
-            ret_coef[i] = self.centered_zero(ret_coef[i], modulo_size)
-                
+        coef_a = np.pad(a.coef, (0, self.params["N"] - len(a.coef)), mode='constant')
+        coef_b = np.pad(b.coef, (0, self.params["N"] - len(b.coef)), mode='constant')
+        ret_coef = (coef_a + coef_b) % modulo_size
+    
         return PolyObj(name, ret_coef)
 
     def subpoly(self, a, b, modulo_size):
-        ret_coef = [self.centered_zero((a.coef[i] - b.coef[i]), modulo_size) for i in range(max(a.degree, b.degree) + 1)]
         
+        ret_coef = [centered_zero((a.coef[i] - b.coef[i]), modulo_size) for i in range(max(len(a.coef), len(b.coef)))]
         return PolyObj("", ret_coef)
 
     def divpoly(self, dividend, division, modulo_size):
         tmp_dptr = PolyObj("", dividend.coef.copy())
         tmp_sptr = PolyObj("", division.coef.copy())
-
-        inv_s = self.inv_of_num(tmp_sptr.coef[tmp_sptr.degree], modulo_size)
+        
+        inv_s = inv_of_num(tmp_sptr.coef[tmp_sptr.degree], modulo_size)
         deg_q = tmp_dptr.degree - tmp_sptr.degree
         q_arr = [0] * self.params["N"]
+
         while tmp_dptr.degree >= tmp_sptr.degree:
-            q_coef = tmp_dptr.coef[tmp_dptr.degree] * inv_s
+            q_coef = centered_zero(tmp_dptr.coef[tmp_dptr.degree] * inv_s, modulo_size)
             for i in range(tmp_sptr.degree + 1):
                 tmp_dptr.coef[i + deg_q] -= tmp_sptr.coef[i] * q_coef
-                tmp_dptr.coef[i + deg_q] %= modulo_size
+                tmp_dptr.coef[i + deg_q] = centered_zero(tmp_dptr.coef[i + deg_q], modulo_size)
             if deg_q >= 0:
                 q_arr[deg_q] = q_coef
                 deg_q -= 1
             while tmp_dptr.coef[tmp_dptr.degree] == 0:
                 tmp_dptr.degree -= 1
+
         if tmp_dptr.degree < 0: 
             tmp_dptr.degree += 1
         
         return [self.poly("quotient", q_arr), tmp_dptr]
-
+        
     def coef_sum(self, Poly):
         return sum(Poly.coef)
 
     def exgcd_poly(self, a, b, modulo_size, name):
-        d = self.poly("", [1])
+        d = self.poly("", [0])
         d1, d2 = self.poly("", [0]), self.poly("", [1])
         tmp_a = self.poly("", (a.coef.copy()))
         tmp_b = self.poly("", (b.coef.copy()))
@@ -176,10 +184,11 @@ class Ntru1:
             d = self.subpoly(d1, self.mulpoly(div, d2, modulo_size, ""), modulo_size)
             tmp_a, tmp_b, d1, d2 = rem, tmp_a, d2, d
         ret = self.poly(name, d.coef)
+
         return ret
     
     def encoder(self, num, name):
-        coef = self.dec2arr(num)
+        coef = dec2arr(num)
         return self.poly(name, coef)
 
     def decoder(self, poly_obj):
@@ -205,6 +214,7 @@ class Ntru1:
     def key_gen(self, g_num):
         
         coef_f = [-1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1]
+        # coef_f = [1, 1, -1]
         self.params["gx"] = self.encoder(g_num, "gx")
         if not self.key_gen_flag:
             self.params["fx"] = self.poly("fx", coef_f)
@@ -220,6 +230,7 @@ class Ntru1:
         print(self.params["Fq"])
         print(self.params["Kp"])
         print(self.params["gx"])
+
         if self.check_key() == -1:
             return -1
         return 1
@@ -234,7 +245,7 @@ class Ntru1:
         ret_poly = self.addpoly(ret_poly, mx, self.params["q"], "cx")
         
         for i in range(self.params["N"]):
-            ret_poly.coef[i] = self.centered_zero(ret_poly.coef[i], self.params["q"])
+            ret_poly.coef[i] = centered_zero(ret_poly.coef[i], self.params["q"])
 
         return ret_poly.coef
 
--- NtruCrypto/main.py
+++ NtruCrypto/main.py
@@ -1,41 +1,93 @@
-from NtruCrypto import PolyObj, Ntru
+from NtruCrypto import Ntru1
+from NTRUCryptoNp import Ntru
 import matplotlib.pyplot as plt
 import time 
-
+import numpy as np
 NTRU_N = 11
 NTRU_p = 3
 NTRU_q = 251
 
 if __name__ == "__main__":
 
+    nt1 = Ntru1(NTRU_N, NTRU_p, NTRU_q)
     nt = Ntru(NTRU_N, NTRU_p, NTRU_q)
+    
     nt.key_gen(3)
+    nt1.key_gen(3)
     err = 0
     times = []
+    times1 = []
+
+    
     for r in range(1 << NTRU_N):
 
-        start_time = time.time_ns()
+        start_time = 0
+        used_time = 0
         count = 0
 
         for i in range(1 << NTRU_N):
-            cx = nt.encrypt(i, 121)
-            mx = nt.decrypt(cx)
+            
+            cx = nt1.encrypt(i, 121)
+            start_time = time.time_ns()
+            mx = nt1.decrypt(cx)
+            end_time = time.time_ns()
             if mx != i:
                 err += 1
+            
+            count+=1
+            c = nt1.poly("", cx)
+            # print(f"m:{i:4d} | cx:{c} | decrypt:{mx:4d}, r:{r} | errs:{err:7d}")
+            used_time += (end_time - start_time)
+
+        times1.append((used_time / count / 1000))
+
+        print(f"Ntru De average spent {(used_time) / count/ 1000:20f} us | r:{r} | errors:{err}")
+        # print(f"Ntru En and De average spent {(end_time - start_time) / count:20f} us")
+
+
+
+
+    for r in range(1 << NTRU_N):
+
+        start_time = 0
+        used_time = 0
+        count = 0
+
+        for i in range(1 << NTRU_N):
+            
+            cx = nt.encrypt(i, 121)
+            start_time = time.time_ns()
+            mx = nt.decrypt(cx)
+            end_time = time.time_ns()
+            if mx != i:
+                err += 1
+            
             count+=1
             c = nt.poly("", cx)
-            # print(f"m:{i:4d} | cx:{c} | decrypt:{mx:4d}, errs:{err:7d}")
-            
-        end_time = time.time_ns()
-        used_time = end_time - start_time
+            # print(f"m:{i:4d} | cx:{c} | decrypt:{mx:4d}, r:{r} | errs:{err:7d}")
+            used_time += (end_time - start_time)
+
         times.append((used_time / count / 1000))
 
-        print(f"Ntru En and De average spent {(end_time - start_time) / count:20f} us | errors:{err}")
-
+        print(f"Ntru De average spent {(used_time) / count/ 1000:20f} us | r:{r} | errors:{err}")
+        # print(f"Ntru En and De average spent {(end_time - start_time) / count:20f} us")
+    
     r_range = range(0, (1 << NTRU_N))
-    plt.plot(r_range, times)
+
+    print(f"ntru1 average spent {np.mean(times1)}us")
+    print(f"ntru average spent {np.mean(times)}us")
+    plt.plot(r_range, times1, color = 'r', label='ntru')
+    plt.plot(r_range, times, color = 'b', label='ntru-conv')
     plt.ylim(0, 1000)
     plt.xlabel('r')
     plt.ylabel('us')
     plt.title('spent time')
     plt.show()
+
+    
+    
+    
+    
+    
+
+    
